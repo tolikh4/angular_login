75cf6647f6b2d469bbf4f93bb2cb178f8d9a5112
tolikh4 <tolikh4@gmail.com>
2019-06-13 13:58:30 +0300

    firebase config

--- loginApp/src/environments/environment.ts
+++ loginApp/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyACKG3lfR0Ja_QE3eseLHuYKNzRIEya2JU",
+    authDomain: "angularlogin-8b79b.firebaseapp.com",
+    databaseURL: "https://angularlogin-8b79b.firebaseio.com",
+    projectId: "angularlogin-8b79b",
+    storageBucket: "angularlogin-8b79b.appspot.com",
+    messagingSenderId: "553096299518",
+    appId: "1:553096299518:web:d0a4aec0e50dc904"
+  },
+  database: 'firebase',
+  socialAuthEnabled: true
 };
 
 /*
