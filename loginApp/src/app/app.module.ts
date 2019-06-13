06db5be9fcc4af3493df72370fc6f67feb3ea76b
tolikh4 <tolikh4@gmail.com>
2019-06-13 14:41:34 +0300

    add modules

--- loginApp/src/app/app.module.ts
+++ loginApp/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'ang_firebase_test'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- loginApp/src/environments/environment.prod.ts
+++ loginApp/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACKG3lfR0Ja_QE3eseLHuYKNzRIEya2JU",
+    authDomain: "angularlogin-8b79b.firebaseapp.com",
+    databaseURL: "https://angularlogin-8b79b.firebaseio.com",
+    projectId: "angularlogin-8b79b",
+    storageBucket: "angularlogin-8b79b.appspot.com",
+    messagingSenderId: "553096299518",
+    appId: "1:553096299518:web:d0a4aec0e50dc904"
+  },
+  database: 'firebase',
+  socialAuthEnabled: true
 };
